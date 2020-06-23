a40a515cdfda5012c62658b944260796e0fe5859
Jenkins <bigbiff@teamw.in>
2020-06-23 18:19:22 +0000

    gps: minimal doesn't have gps package

--- device_aries.mk
+++ device_aries.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product-if-exists, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product-if-exists, vendor/xiaomi/aries/aries-vendor.mk)
 
