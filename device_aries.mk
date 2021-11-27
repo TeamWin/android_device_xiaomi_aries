47b805a3d9b7e3f19e7007f7b71b88687e72f329
bigbiff <bigbiff@teamw.in>
2021-11-27 02:33:43 +0000

    aries: remove goldfish dependency

--- device_aries.mk
+++ device_aries.mk
@@ -20,7 +20,7 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_aries
